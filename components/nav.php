1af9f3564a9a54976dd87a4acbc540382cef6441
N Twice Company <ntwicecompany@gmail.com>
2026-03-17 10:11:12 +0100

    Add navigation bar to components/nav.php

---
+++ components/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <div class="container">
+        <b>Malm Pay</b>
+        <a href="#">Home</a>
+        <a href="#">Customer</a>
+        <a href="#">Business</a>
+        <a href="#">Career</a>
+        <a href="#">About</a>
+    </div>
+</nav>
