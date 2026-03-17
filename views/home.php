7cbb864e8e083d19501fc2d1863176b142b9d496
N Twice Company <ntwicecompany@gmail.com>
2026-03-17 10:10:22 +0100

    Add home.php for the main landing page

---
+++ views/home.php
@@ -0,0 +1,38 @@
+<html>
+    <body>
+        <?php include "./components/nav.php"; ?>
+
+        <header>
+            <div class="container">
+                <h1>Welcome to Malm Pay!</h1>
+                <p>Shop online secure.</p>
+            </div>
+        </header>
+
+        <section>
+            <div class="container">
+                <h2>Secure Checkout</h2>
+                <p>Your economy is protected by industry leading technology.</p>
+                <a href="#">Join as customer</a>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <h2>For Online Businesses</h2>
+                <p>Simplify your operations and deliver a seamless experience to your customers.</p>
+                <a href="#">Join as business</a>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <h2>Become a team member</h2>
+                <p>Be part of something we’re building from the ground up. Grow with us and make an impact.</p>
+                <a href="#">Join our team</a>
+            </div>
+        </section>
+
+        <?php include "./components/footer.php"; ?>
+    </body>
+</html>
